980e12aad702ade9fc8f167fc561f4db3c469a46
cocoapuffs123 <122548185+cocoapuffs123@users.noreply.github.com>
2023-01-20 11:03:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,18 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
+    music.setVolume(255)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("bye")
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showString("idk?")
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C5 C D G B E A G ", 242)
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showString("wassup guys.")
